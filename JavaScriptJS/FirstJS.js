bc8fea354cb80987d7baad080f8cafc907947dbe
natarajn121 <38654182+natarajn121@users.noreply.github.com>
2023-10-13 06:04:51 +0000

    Version 2

--- JavaScriptJS/FirstJS.js
+++ JavaScriptJS/FirstJS.js
@@ -1,1 +1,37 @@
 console.log("Nataraj")
+let score = 20
+
+ 
+
+console.log(typeof score);
+
+console.log(typeof (score));
+
+ 
+
+let valueIn = Number(score)
+
+ 
+
+console.log(typeof valueIn);
+
+console.log(valueIn);
+
+
+score = undefined
+
+ 
+
+console.log(typeof score);
+
+console.log(typeof (score));
+
+ 
+
+ valueIn = Number(score)
+
+ 
+
+console.log(typeof valueIn);
+
+console.log(valueIn);
